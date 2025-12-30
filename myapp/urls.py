b2569ce473e451ac4cebe895245b42c8e1f1739d
Bikram Sah <88473898+bikramsah@users.noreply.github.com>
2025-12-30 16:16:27 +0545

    Update URL paths for Khalti views

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
+
 urlpatterns = [
-    
-    path('',views.home,name="home"),
-    path('initiate',views.initkhalti,name="initiate"),
-    path('verify',views.verifyKhalti,name="verify")
+    path('', views.home, name='home'),
+    path('initiate/', views.initkhalti, name='initiate'),
+    path('verify/', views.verifyKhalti, name='verify'),
 ]
